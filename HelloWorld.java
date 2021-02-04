9bdc7ff9d050335fc285f685ddda64a0f6ec4a48
gvelazc0 <69766792+gvelazc0@users.noreply.github.com>
2021-02-03 21:36:29 -0500

    Revert "Program modified"
    This reverts commit 5dcebfa24fc54e105c509887310283b690540da9.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,7 @@
 public class HelloWorld
 {
-   public static void main(String[] args)
+   public static void main(String[] args) throws IOException
    {
       System.out.println("Hello World");
-      System.out.println("Program modified, but able to revert to original version using GitHub Desktop");      
    }
 }
